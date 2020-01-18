101ac9269a123159c1ff3754319d44c60723baa8
congcong1994 <dl_congcong@163.com>
2020-01-18 15:38:44 +0800

    图片懒加载

--- 飞鹏网首页改版/飞鹏网首页改版/js/index.js
+++ 飞鹏网首页改版/飞鹏网首页改版/js/index.js
@@ -75,4 +75,39 @@ $(function() {
       .eq(nextIndex)
       .addClass('active');
   });
+
+  // 图片懒加载
+  lazyRender();
+  var clock;
+  $(window).on('scroll', function() {
+    if (clock) {
+      clearTimeout(clock);
+    }
+    clock = setTimeout(function() {
+      lazyRender();
+    }, 300);
+  });
+  function lazyRender() {
+    $('body img.lazy-load-img').each(function() {
+      if (checkShow($(this)) && !isLoaded($(this))) {
+        loadImg($(this));
+      }
+    });
+  }
+  function checkShow($img) {
+    var scrollTop = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var offsetTop = $img.offset().top;
+
+    if (offsetTop < scrollTop + windowHeight && offsetTop >= scrollTop) {
+      return true;
+    }
+    return false;
+  }
+  function isLoaded($img) {
+    return $img.attr('data-src') === $img.attr('src');
+  }
+  function loadImg($img) {
+    $img.attr('src', $img.attr('data-src'));
+  }
 });
